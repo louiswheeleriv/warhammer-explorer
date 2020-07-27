3ba43fcbb8f4dbe8b00c09df45aa1fbf0085e1d5
Louis Wheeler <louiswheeleriv@gmail.com>
2020-07-27 12:08:46 -0400

    Include jquery, stub index js file

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_directory ../javascripts .js
 
 //= link graphiql/rails/application.css
 //= link graphiql/rails/application.js
