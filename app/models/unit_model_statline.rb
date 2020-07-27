1bea6b7c3a40ffa175d88cfc2c3b2ced08695b04
Louis Wheeler <louiswheeleriv@gmail.com>
2020-07-27 10:39:30 -0400

    Wound track validation on statline

--- app/models/unit_model_statline.rb
+++ app/models/unit_model_statline.rb
@@ -20,4 +20,28 @@
 #
 class UnitModelStatline < ApplicationRecord
 	belongs_to :unit_model
+
+	FIELDS_REQUIRED_UNLESS_WOUND_TRACK = [
+		:movement, :weapon_skill, :ballistic_skill, :strength,
+		:toughness, :wounds, :attacks, :leadership, :armor_save
+	].freeze
+
+	FIELDS_REQUIRED_UNLESS_WOUND_TRACK.each do |field|
+		validates field, presence: true, unless: Proc.new { |stats| stats.wound_track? }
+	end
+
+	validate :wound_track_range_validation
+
+	def wound_track?
+		!!wound_track_bottom && !!wound_track_top
+	end
+
+	private
+
+	def wound_track_range_validation
+		if !!wound_track_bottom ^ !!wound_track_top
+			errors.add(:base, "If using wound track, both wound_track_bottom and wound_track_top are required.")
+		end
+	end
+
 end
